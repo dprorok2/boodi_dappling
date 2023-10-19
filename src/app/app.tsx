d5f2115f955e6a7e66da12e11240375a36e9b1a1
Alex Crist <alex@alex-crist.com>
2023-10-19 09:53:57 -0500

    Welcome component

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import Welcome from './welcome/welcome';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="boodi" />
+      <Welcome />
     </div>
   );
 }
