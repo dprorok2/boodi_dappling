964e4ddda9e5217ae964bf5946d0f89eb8d9dcba
Alex Crist <alex@alex-crist.com>
2023-11-11 21:32:11 -0600

    Update api-urls.ts

--- libs/services/src/api-urls.ts
+++ libs/services/src/api-urls.ts
@@ -1,9 +1,10 @@
 //const BASE = 'http://localhost:1104';
 const BASE = 'https://boodi-proxy.replit.app';
+const BASE_WS = 'ws://boodi-proxy.replit.app';
 
 export const API_URLS = {
   proxy: {
-    fourNobleTruths: `${BASE}/four-noble-truths`,
+    fourNobleTruths: `${BASE_WS}/four-noble-truths`,
     eightfoldPath: `${BASE}/eightfold-path`,
     eightfoldPathFullStreaming: `${BASE}/eightfold-path/full-streaming`,
   },
